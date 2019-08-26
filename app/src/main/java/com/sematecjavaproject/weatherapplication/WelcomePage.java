bc45543d7af4f9509bf5d98c4093fd06f6a0b7df
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-27 02:04:24 +0430

    internet connection checking completed

--- app/src/main/java/com/sematecjavaproject/weatherapplication/WelcomePage.java
+++ app/src/main/java/com/sematecjavaproject/weatherapplication/WelcomePage.java
@@ -1,14 +1,79 @@
 package com.sematecjavaproject.weatherapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class WelcomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.welcome_page);
+
+        if (!isConnected(WelcomePage.this))
+            buildDialog(WelcomePage.this).show();
+        else
+            setContentView(R.layout.welcome_page);
     }
+
+    public boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting()))
+                return true;
+            else return false;
+        } else
+            return false;
+    }
+
+    public AlertDialog.Builder buildDialog(Context c) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Please make sure you are connected to the internet." + "\n" + "Press ok to Exit");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
+            }
+        });
+
+        return builder;
+    }
+
+//    private boolean isNetworkConnected() {
+//
+//        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(WelcomePage.CONNECTIVITY_SERVICE);
+//        return connectivityManager.getActiveNetworkInfo() != null & connectivityManager.getActiveNetworkInfo().isConnected();
+//    }
+
+//    public boolean isInternetAvailable() {
+//
+//        try {
+//
+//            InetAddress ipAddr = InetAddress.getByName("google.com");
+//            return !ipAddr.equals("");
+//        } catch (Exception e) {
+//
+//            return false;
+//        }
+//    }
 }
