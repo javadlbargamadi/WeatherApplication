f0d93718131d4533d1011699027d30e4bdb0540d
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-27 01:14:10 +0430

    first comm in end of welcome page design

---
+++ app/src/main/java/com/sematecjavaproject/weatherapplication/WelcomePage.java
@@ -0,0 +1,14 @@
+package com.sematecjavaproject.weatherapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+    }
+}
