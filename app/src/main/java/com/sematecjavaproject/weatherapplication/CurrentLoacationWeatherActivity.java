ba881e40c596635cde3034d552724b34bd8a9428
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-27 15:22:06 +0430

    search page created and intented

--- app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
+++ app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +20,19 @@ public class CurrentLoacationWeatherActivity extends AppCompatActivity {
         Toast.makeText(CurrentLoacationWeatherActivity.this, "in down section", Toast.LENGTH_LONG).show();
         Toast.makeText(CurrentLoacationWeatherActivity.this, "swipe left", Toast.LENGTH_LONG).show();
 
+        TextView txtChangeCity = findViewById(R.id.txtChangeCity);
         TextView textView = findViewById(R.id.txtTest);
         RecyclerView recyclerView = findViewById(R.id.OtherDaysRecyclerView);
+
+        txtChangeCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent changeCityIntent = new Intent(CurrentLoacationWeatherActivity.this, SearchCityActivity.class);
+                startActivity(changeCityIntent);
+            }
+        });
+
 
         ItemAdapterActivity itemAdapterActivity = new ItemAdapterActivity();
         recyclerView.setAdapter(itemAdapterActivity);
