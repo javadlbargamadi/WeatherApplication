6f956b080c2e616999ed499d149f609a828674e8
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-28 02:23:43 +0430

    in json

--- app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
+++ app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
@@ -12,12 +12,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.sematecjavaproject.weatherapplication.Weather.Weather;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class CurrentLoacationWeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_loacation_weather);
+
+        String url;
+        final Double mainTemp;
 
         Toast.makeText(CurrentLoacationWeatherActivity.this, "open drawer to change city", Toast.LENGTH_SHORT).show();
 
@@ -37,8 +49,26 @@ public class CurrentLoacationWeatherActivity extends AppCompatActivity {
         TextView txtSunrise = findViewById(R.id.txtSunrise);
         TextView txtSunset = findViewById(R.id.txtSunset);
 
+        url = "http://api.openweathermap.org/data/2.5/find?q=Tehran&units=metric&APPID=05c1d09d0ef22e9f6ddfbf67d8dcc151";
 
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.get(url, new JsonHttpResponseHandler(){
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+
+                Gson gson = new Gson();
+
+                Weather weather = gson.fromJson(response.toString(), Weather.class);
+                mainTemp = weather.getMain().ge;
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
 
 
         TextView txtChangeCity = findViewById(R.id.txtChangeCity);
