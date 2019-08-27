a080ff590cdf48beaeff08d48fa2c75bbda89572
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-28 00:12:35 +0430

    ids finded by id in current weather

--- app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
+++ app/src/main/java/com/sematecjavaproject/weatherapplication/CurrentLoacationWeatherActivity.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +20,26 @@ public class CurrentLoacationWeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_current_loacation_weather);
 
         Toast.makeText(CurrentLoacationWeatherActivity.this, "open drawer to change city", Toast.LENGTH_SHORT).show();
+
+
+
+
+        TextView txtCity = findViewById(R.id.txtCity);
+        TextView txtWeatherType = findViewById(R.id.txtWeatherType);
+        TextView txtWeatherDescription = findViewById(R.id.txtWeatherDescription);
+
+        ImageView imgWeatherIcon = findViewById(R.id.imgWeatherIcon);
+
+        TextView txtMinTemp = findViewById(R.id.txtMinTemp);
+        TextView txtMaxTemp = findViewById(R.id.txtMaxTemp);
+        TextView txtMainTemp = findViewById(R.id.txtMainTemp);
+        TextView txtWindSpeed = findViewById(R.id.txtWindSpeed);
+        TextView txtSunrise = findViewById(R.id.txtSunrise);
+        TextView txtSunset = findViewById(R.id.txtSunset);
+
+
+
+
 
         TextView txtChangeCity = findViewById(R.id.txtChangeCity);
         TextView textView = findViewById(R.id.txtTest);
